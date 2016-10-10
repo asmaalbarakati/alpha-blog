9e72458f0b1f0aba46239a52b76a54b61c7ddb8a
completerubyonrailscourse <albarakatiasma@hotmail.com>
2016-10-10 19:14:25 +0000

    add index,listi,edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,41 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
   end  
   
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
-    
+    if @article.save
+      flash[:notice]="Article saved successfully."
+      redirect_to article_path(@article)
+    else #validation error  
+     render 'new'
+    end
   end  
   
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice]="Article edited successfully."
+      redirect_to article_path(@article)
+    else #validation error  
+     render 'edit'
+    end 
+  end 
+  
+  def show
+    @article = Article.find(params[:id])
+  end  
+    
   private
    def article_params
      params.require(:article).permit(:title, :description)
